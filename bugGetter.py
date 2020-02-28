8a35c2fcb8d5f1d77fa90bf32ac8369168d6d82d
xf97 <15850673022@163.com>
2020-02-28 16:22:06 +0800

    update code

--- bugGetter.py
+++ bugGetter.py
@@ -9,14 +9,18 @@ and Github.
 
 #import library
 import requests 
+from bs4 import BeautifulSoup
+import re
+import json
 
-https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=smart%20contract%20bugs&highlight=true&returnFacets=ALL&returnType=SEARCH&refinements=ContentType:Conferences&refinements=ContentType:Journals
+#https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=smart%20contract%20bugs&highlight=true&returnFacets=ALL&returnType=SEARCH&refinements=ContentType:Conferences&refinements=ContentType:Journals
 
 #define url and text
 #eg. https://github.com/search?q=smart+contract+bugs
 GIT_TYPE = 1
 GIT_SEARCH_TEXT = ""
-GIT_SEARCH = "https://github.com/search?q="
+GIT_SEARCH = "https://api.github.com/search/repositories?q="
+GIT_SUFFIX = "&per_page="
 #eg. https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=smart%20contract%20bugs&highlight=true&returnFacets=ALL&returnType=SEARCH&refinements=ContentType:Conferences&refinements=ContentType:Journals
 IEEE_TYPE = 2
 IEEE_SEARCH_TEXT = ""
@@ -43,6 +47,61 @@ def getPage(_url, _text, _type):
 	if _type == GIT_TYPE:
 		text = _text.replace(" ", "+")
 		search_text += text
-	r =requests.get(_)
+	r = getHtml(search_text)
+	if r:
+		return r, search_text
+	else:
+		return None
 
+#This function is used to get html code
+def getHtml(_url):
+	headers = {'user_agent': 'Mozilla/5.0'}
+	r = requests.get(_url, headers = headers)
+	r.raise_for_status()
+	r.encoding = r.apparent_encoding
+	return r
+
+
+#This function used BeautifulSoup to parser html
+def parserHtml(_page, _type):
+	soup = BeautifulSoup(_page, "html.parser")
+	if _type == GIT_TYPE:	
+		#获取项目数
+		n_pattern = "<h3>(.)*<>"
+		print(soup.find_all("h3", string = re.compile("results")))
+		print(len(soup.find_all("a")))
+
+#This function uses re to get target string
+def parserHtml_re(_text, _url,  _type):
+	if _type == GIT_TYPE:
+		proInfo = dict()
+		#转变数据格式
+		page_dict = _text.json()
+		#get project number to computer the number of page
+		proNum = page_dict["total_count"]
+		if proNum < 30:
+			getGithubProInfo(page_dict, proInfo)
+		while proNum >= 0:
+			getGithubProInfo(page_dict, proInfo)
+			proNum -= 30
+			search_text = _url
+			if proNum <= 100:
+				search_text += "&per_page=100"
+			for key in proInfo.keys():
+				print(key, proInfo[key])
+
+
+
+#This function is used to get Githun project name
+def getGithubProInfo(_dict, _proInfo):
+	#this is list
+	for item in _dict["items"]:
+		for key in item.keys():
+			_proInfo[item["full_name"]] = item["updated_at"] 
+	return 
+
+
+#test code
+(page, url) = getPage(GIT_SEARCH, KEYWORD5, GIT_TYPE)
+parserHtml_re(page, url, GIT_TYPE)
  
